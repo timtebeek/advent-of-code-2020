e0951faf8ee6dc16148e18b83792575b56c96bc5
Tim te Beek <tim.te.beek@jdriven.com>
2020-12-06 10:47:15 +0100

    Simplify part1

--- src/test/java/com/github/timtebeek/advent2020/day06/ImmigrationFormTest.java
+++ src/test/java/com/github/timtebeek/advent2020/day06/ImmigrationFormTest.java
@@ -8,6 +8,7 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
@@ -17,48 +18,46 @@ class ImmigrationFormTest {
 
     @Test
     void testSample1() throws Exception {
-        List<List<String>> forms = parseImmigrationForms("sample1.txt");
-        assertThat(sumAnyYesPerGroup(forms)).isEqualByComparingTo(11L);
+        assertThat(parseImmigrationFormsToStream("sample1.txt")
+                .mapToLong(group -> group.distinct().count()).sum())
+                        .isEqualByComparingTo(11L);
     }
 
     @Test
     void testPart1() throws Exception {
-        List<List<String>> forms = parseImmigrationForms("part1.txt");
-        assertThat(sumAnyYesPerGroup(forms)).isEqualByComparingTo(6382L);
+        assertThat(parseImmigrationFormsToStream("part1.txt")
+                .mapToLong(group -> group.distinct().count()).sum())
+                        .isEqualByComparingTo(6382L);
     }
 
     @Test
     void testSample2() throws Exception {
-        List<List<String>> forms = parseImmigrationForms("sample1.txt");
+        List<List<String>> forms = parseImmigrationFormsToList("sample1.txt");
         assertThat(sumAllYesPerGroup(forms)).isEqualByComparingTo(6L);
     }
 
     @Test
     void testPart2() throws Exception {
-        List<List<String>> forms = parseImmigrationForms("part1.txt");
+        List<List<String>> forms = parseImmigrationFormsToList("part1.txt");
         assertThat(sumAllYesPerGroup(forms)).isEqualByComparingTo(3197L);
     }
 
-    private List<List<String>> parseImmigrationForms(String filename) throws Exception {
+    private Stream<IntStream> parseImmigrationFormsToStream(String filename) throws Exception {
+        Path input = Paths.get(getClass().getResource(filename).toURI());
+        String batch = Files.readString(input);
+        return Stream.of(batch.split("\n\n"))
+                .map(str -> str.split("\n"))
+                .map(Stream::of)
+                .map(group -> group.flatMapToInt(String::chars));
+    }
+
+    private List<List<String>> parseImmigrationFormsToList(String filename) throws Exception {
         Path input = Paths.get(getClass().getResource(filename).toURI());
         String batch = Files.readString(input);
         return Stream.of(batch.split("\n\n"))
                 .map(str -> str.split("\n"))
                 .map(List::of)
                 .collect(toList());
-    }
-
-    private static long sumAnyYesPerGroup(List<List<String>> forms) {
-        return forms.stream()
-                .mapToLong(ImmigrationFormTest::countAnyYesPerGroup)
-                .sum();
-    }
-
-    private static long countAnyYesPerGroup(List<String> group) {
-        return group.stream()
-                .flatMapToInt(String::chars)
-                .distinct()
-                .count();
     }
 
     private static long sumAllYesPerGroup(List<List<String>> forms) {
