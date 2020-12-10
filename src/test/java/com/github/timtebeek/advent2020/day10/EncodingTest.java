bd3d269dd4b3dd91b6426c27ac352cc56539755b
Tim te Beek <tim.te.beek@jdriven.com>
2020-12-10 09:20:06 +0100

    Day 10 part 1

---
+++ src/test/java/com/github/timtebeek/advent2020/day10/EncodingTest.java
@@ -0,0 +1,91 @@
+package com.github.timtebeek.advent2020.day10;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EncodingTest {
+
+    @Test
+    void testMultipliedDifferencesSample1() throws Exception {
+        List<Integer> numbers = parse("sample1.txt");
+        assertThat(findMultipliedDifferences(numbers)).isEqualByComparingTo(7 * 5);
+    }
+
+    @Test
+    void testMultipliedDifferencesSample2() throws Exception {
+        List<Integer> numbers = parse("sample2.txt");
+        assertThat(findMultipliedDifferences(numbers)).isEqualByComparingTo(22 * 10);
+    }
+
+    @Test
+    void testMultipliedDifferencesPart1() throws Exception {
+        List<Integer> numbers = parse("part1.txt");
+        assertThat(findMultipliedDifferences(numbers)).isEqualByComparingTo(2590);
+    }
+
+    @Test
+    void testDistinctArrangementsSample1() throws Exception {
+        List<Integer> numbers = parse("sample1.txt");
+        assertThat(findDistinctArrangements(numbers)).isEqualByComparingTo(8L);
+    }
+
+    @Test
+    void testDistinctArrangementsSample2() throws Exception {
+        List<Integer> numbers = parse("sample2.txt");
+        assertThat(findDistinctArrangements(numbers)).isEqualByComparingTo(19208L);
+    }
+
+    @Test
+    void testDistinctArrangementsPart1() throws Exception {
+        List<Integer> numbers = parse("part1.txt");
+        assertThat(findDistinctArrangements(numbers)).isEqualByComparingTo(-1L);
+    }
+
+    private List<Integer> parse(String filename) throws URISyntaxException, IOException {
+        Path input = Paths.get(getClass().getResource(filename).toURI());
+        List<Integer> parsed = Files.readAllLines(input).stream()
+                .map(Integer::valueOf)
+                .sorted()
+                .collect(toList());
+        // Add outlet and device
+        parsed.add(0, 0);
+        parsed.add(parsed.stream().mapToInt(Integer::intValue).max().getAsInt() + 3);
+        return parsed;
+    }
+
+    private static int findMultipliedDifferences(List<Integer> numbers) {
+        List<Integer> differences = new ArrayList<>();
+        for (int i = 0; i < numbers.size() - 1; i++) {
+            differences.add(numbers.get(i + 1) - numbers.get(i));
+        }
+        Map<Integer, List<Integer>> collect = differences.stream()
+                .collect(Collectors.groupingBy(Function.identity()));
+        return collect.get(1).size() * collect.get(3).size();
+    }
+
+    private long findDistinctArrangements(List<Integer> numbers) {
+        if (numbers.size() == 1) {
+            return 1;
+        }
+        Integer jolts = numbers.get(0);
+        int plusOne = numbers.indexOf(jolts + 1);
+        int plusTwo = numbers.indexOf(jolts + 2);
+        int plusThree = numbers.indexOf(jolts + 3);
+        return (plusOne != -1 ? findDistinctArrangements(numbers.subList(plusOne, numbers.size())) : 0)
+                + (plusTwo != -1 ? findDistinctArrangements(numbers.subList(plusTwo, numbers.size())) : 0)
+                + (plusThree != -1 ? findDistinctArrangements(numbers.subList(plusThree, numbers.size())) : 0);
+    }
+}
