74afb3e20e42913132ded1ea276bdb4918eec860
Tim te Beek <tim.te.beek@jdriven.com>
2020-12-20 21:12:41 +0100

    Replace Map with Set

--- src/test/java/com/github/timtebeek/advent2020/day17/ConwayCubesTest.java
+++ src/test/java/com/github/timtebeek/advent2020/day17/ConwayCubesTest.java
@@ -4,8 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Value;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 import static com.github.timtebeek.advent2020.day17.Dimensions.FOUR;
 import static com.github.timtebeek.advent2020.day17.Dimensions.THREE;
@@ -32,39 +32,39 @@ class ConwayCubesTest {
 
     @Test
     void testSample1() {
-        Map<Coordinate, Boolean> map = parse(SAMPLE1, THREE);
-        Map<Coordinate, Boolean> endState = cycle(map, 6, THREE);
+        Set<Coordinate> map = parse(SAMPLE1, THREE);
+        Set<Coordinate> endState = cycle(map, 6, THREE);
         assertThat(endState).hasSize(112);
     }
 
     @Test
     void testPart1() {
-        Map<Coordinate, Boolean> map = parse(PART1, THREE);
-        Map<Coordinate, Boolean> endState = cycle(map, 6, THREE);
+        Set<Coordinate> map = parse(PART1, THREE);
+        Set<Coordinate> endState = cycle(map, 6, THREE);
         assertThat(endState).hasSize(284);
     }
 
     @Test
     void testSample2() {
-        Map<Coordinate, Boolean> map = parse(SAMPLE1, FOUR);
-        Map<Coordinate, Boolean> endState = cycle(map, 6, FOUR);
+        Set<Coordinate> map = parse(SAMPLE1, FOUR);
+        Set<Coordinate> endState = cycle(map, 6, FOUR);
         assertThat(endState).hasSize(848);
     }
 
     @Test
     void testPart2() {
-        Map<Coordinate, Boolean> map = parse(PART1, FOUR);
-        Map<Coordinate, Boolean> endState = cycle(map, 6, FOUR);
+        Set<Coordinate> map = parse(PART1, FOUR);
+        Set<Coordinate> endState = cycle(map, 6, FOUR);
         assertThat(endState).hasSize(2240);
     }
 
-    private static Map<Coordinate, Boolean> parse(String slice, Dimensions dimensions) {
-        Map<Coordinate, Boolean> map = new HashMap<>();
+    private static Set<Coordinate> parse(String slice, Dimensions dimensions) {
+        Set<Coordinate> map = new HashSet<>();
         String[] split = slice.split("\n");
         for (int y = 0; y < split.length; y++) {
             for (int x = 0; x < split[0].length(); x++) {
                 if (split[y].charAt(x) == '#') {
-                    map.put(new Coordinate(x, y, dimensions), true);
+                    map.add(new Coordinate(x, y, dimensions));
                 }
             }
         }
@@ -72,9 +72,9 @@ class ConwayCubesTest {
         return map;
     }
 
-    private static Map<Coordinate, Boolean> cycle(Map<Coordinate, Boolean> initialState, int cycles,
+    private static Set<Coordinate> cycle(Set<Coordinate> initialState, int cycles,
             Dimensions dimensions) {
-        Map<Coordinate, Boolean> currentState = new HashMap<>(initialState);
+        Set<Coordinate> currentState = new HashSet<>(initialState);
         for (int cycle = 0; cycle < cycles; cycle++) {
             currentState = Coordinate.cycleOnce(currentState, dimensions);
         }
@@ -101,20 +101,20 @@ class Coordinate {
         this.dimensions = dimensions;
     }
 
-    private int countActiveNeighbours(Map<Coordinate, Boolean> state) {
+    private int countActiveNeighbours(Set<Coordinate> state) {
         int activeNeighbours = 0;
         for (int otherX = x - 1; otherX <= x + 1; otherX++) {
             for (int otherY = y - 1; otherY <= y + 1; otherY++) {
                 for (int otherZ = z - 1; otherZ <= z + 1; otherZ++) {
                     if (dimensions == THREE) {
                         Coordinate f = new Coordinate(otherX, otherY, otherZ, 0, dimensions);
-                        if (!this.equals(f) && state.getOrDefault(f, false)) {
+                        if (!this.equals(f) && state.contains(f)) {
                             activeNeighbours++;
                         }
                     } else {
                         for (int otherW = w - 1; otherW <= w + 1; otherW++) {
                             Coordinate f = new Coordinate(otherX, otherY, otherZ, otherW, dimensions);
-                            if (!this.equals(f) && state.getOrDefault(f, false)) {
+                            if (!this.equals(f) && state.contains(f)) {
                                 activeNeighbours++;
                             }
                         }
@@ -125,25 +125,25 @@ class Coordinate {
         return activeNeighbours;
     }
 
-    public static Map<Coordinate, Boolean> cycleOnce(Map<Coordinate, Boolean> currentState, Dimensions dimensions) {
-        var xs = currentState.keySet().stream().map(Coordinate::getX).collect(toSet());
-        var ys = currentState.keySet().stream().map(Coordinate::getY).collect(toSet());
-        var zs = currentState.keySet().stream().map(Coordinate::getZ).collect(toSet());
-        var ws = currentState.keySet().stream().map(Coordinate::getW).collect(toSet());
-        Map<Coordinate, Boolean> nextState = new HashMap<>();
+    public static Set<Coordinate> cycleOnce(Set<Coordinate> currentState, Dimensions dimensions) {
+        var xs = currentState.stream().map(Coordinate::getX).collect(toSet());
+        var ys = currentState.stream().map(Coordinate::getY).collect(toSet());
+        var zs = currentState.stream().map(Coordinate::getZ).collect(toSet());
+        var ws = currentState.stream().map(Coordinate::getW).collect(toSet());
+        Set<Coordinate> nextState = new HashSet<>();
         for (int z = min(zs) - 1; z <= max(zs) + 1; z++) {
             for (int y = min(ys) - 1; y <= max(ys) + 1; y++) {
                 for (int x = min(xs) - 1; x <= max(xs) + 1; x++) {
                     if (dimensions == THREE) {
                         Coordinate coordinate = new Coordinate(x, y, z, 0, dimensions);
                         if (coordinateActiveNextCycle(coordinate, currentState)) {
-                            nextState.put(coordinate, true);
+                            nextState.add(coordinate);
                         }
                     } else {
                         for (int w = min(ws) - 1; w <= max(ws) + 1; w++) {
                             Coordinate coordinate = new Coordinate(x, y, z, w, dimensions);
                             if (coordinateActiveNextCycle(coordinate, currentState)) {
-                                nextState.put(coordinate, true);
+                                nextState.add(coordinate);
                             }
                         }
                     }
@@ -153,8 +153,8 @@ class Coordinate {
         return nextState;
     }
 
-    static boolean coordinateActiveNextCycle(Coordinate coordinate, Map<Coordinate, Boolean> state) {
-        boolean currentlyActive = state.getOrDefault(coordinate, false);
+    static boolean coordinateActiveNextCycle(Coordinate coordinate, Set<Coordinate> state) {
+        boolean currentlyActive = state.contains(coordinate);
         int activeNeighbours = coordinate.countActiveNeighbours(state);
         return currentlyActive && activeNeighbours == 2 || activeNeighbours == 3;
     }
